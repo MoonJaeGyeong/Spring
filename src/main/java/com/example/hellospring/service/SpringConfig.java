6f9f1a7ea97893caf9a5e2959e3c74939c13270d
MoonJaeGyeong <bblbblan69@gmail.com>
2023-07-27 23:18:14 +0900

    Section 6
    Spring DB 접근 기술

--- src/main/java/com/example/hellospring/service/SpringConfig.java
+++ src/main/java/com/example/hellospring/service/SpringConfig.java
@@ -1,20 +1,28 @@
 package com.example.hellospring.service;
 
+import com.example.hellospring.repository.JdbcTemplateMemberRepository;
 import com.example.hellospring.repository.MemberRepository;
-import com.example.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
-
+    private final DataSource dataSource;
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
     }
-
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+       // return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
