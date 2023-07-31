a2b7238a9abe66a3cef67ae376f315a43d65b527
MoonJaeGyeong <bblbblan69@gmail.com>
2023-07-31 15:48:40 +0900

    Section 6
    JPA 와 스프링 데이터 JPA

--- src/main/java/com/example/hellospring/service/SpringConfig.java
+++ src/main/java/com/example/hellospring/service/SpringConfig.java
@@ -1,28 +1,29 @@
 package com.example.hellospring.service;
 
-import com.example.hellospring.repository.JdbcTemplateMemberRepository;
 import com.example.hellospring.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
-
 @Configuration
 public class SpringConfig {
-    private final DataSource dataSource;
+
+    private final MemberRepository memberRepository;
     @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
+
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
-    @Bean
-    public MemberRepository memberRepository() {
-        //return new MemoryMemberRepository();
-       // return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
-    }
+
+//    @Bean
+//    public MemberRepository memberRepository() {
+//       return new MemoryMemberRepository();
+//       return new JdbcMemberRepository(dataSource);
+//       return new JdbcTemplateMemberRepository(dataSource);
+//       return new JpaMemberRepository(em);
+//    }
 }
