70819468b73f5c14f61f116fc5daa5c6d94edd51
MoonJaeGyeong <bblbblan69@gmail.com>
2023-08-05 00:15:14 +0900

    move_repository

---
+++ hello-spring_introduction/src/main/java/com/example/hellospring/service/MemberService.java
@@ -0,0 +1,48 @@
+package com.example.hellospring.service;
+
+import com.example.hellospring.domain.Member;
+import com.example.hellospring.repository.MemberRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Transactional
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+
+    /**
+    *Join
+     */
+    public Long join(Member member){
+            // 이름 중복 회원
+            validateDuplicateMember(member);
+            memberRepository.save(member);
+            return member.getId();
+
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberID){
+        return memberRepository.findById(memberID);
+    }
+}
