35b02fa32e64a00b5f18198374b830e831745a64
MoonJaeGyeong <bblbblan69@gmail.com>
2023-08-30 00:20:19 +0900

    Section 6
    Component Scan

---
+++ springcore/src/main/java/hello/springcore/AutoAppConfig.java
@@ -0,0 +1,15 @@
+package hello.springcore;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        basePackages = "hello.springcore",
+        basePackageClasses = AutoAppConfig.class,
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+
+}
